50adf5a61b22550386488796fbb7889b0c8b96e0
dev <valex>
2021-07-23 01:11:25 +0300

    more

--- Deitel P., Deitel H. - Intro to Python for Computer Science and Data Science - 2020/07_overview.py
+++ Deitel P., Deitel H. - Intro to Python for Computer Science and Data Science - 2020/07_overview.py
@@ -1,11 +1,222 @@
 import numpy as np
 import random
+import pandas as pd
+
+# ----------------------------------------------------
+# DataFrames (enhanced two-dimensional array)
+# ----------------------------------------------------
+
+
+# ----------------------------------------------------
+# Series (enhanced one-dimensional array)
+# ----------------------------------------------------
+
+# grades = pd.Series([87, 100, 94])
+
+# print( grades )
+# # Output:
+# # 0     87
+# # 1    100
+# # 2     94
+# # dtype: int64
+
+# print( pd.Series(98.6, range(3)) )
+# # Output:
+# # 0    98.6
+# # 1    98.6
+# # 2    98.6
+# # dtype: float64
+
+# print( grades[0] )
+# print( grades.count() )
+# print( grades.mean() )
+# print( grades.min() )
+# print( grades.max() )
+# print( grades.std() )
+
+# print( grades.describe() )
+# # Output:
+# # count      3.000000
+# # mean      93.666667
+# # std        6.506407
+# # min       87.000000
+# # 25%       90.500000
+# # 50%       94.000000
+# # 75%       97.000000
+# # max      100.000000
+# # dtype: float64
+
+# grades = pd.Series([87,100,94], index=["Wally", "Eva", "Sam"])
+# print( grades )
+# # Output:
+# # Wally     87
+# # Eva      100
+# # Sam       94
+# # dtype: int64
+
+# grades = pd.Series({'Wally':87, 'Eva':100, 'Sam':94})
+# print( grades )
+# # Output:
+# # Wally     87
+# # Eva      100
+# # Sam       94
+# # dtype: int64
+
+# print( grades['Eva'] )
+# print( grades.Wally )
+
+# print( grades.dtype )
+
+# print( grades.values )
+# # Output:
+# # [ 87 100  94]
+
+
+# hardware = pd.Series(['Hammer', 'Saw', 'Wrench'])
+# print(hardware)
+# # Output:
+# # 0    Hammer
+# # 1       Saw
+# # 2    Wrench
+# # dtype: object
+
+# print( hardware.str.contains('a') )
+# # Output:
+# # 0     True
+# # 1     True
+# # 2    False
+# # dtype: bool
+
+# print( hardware.str.upper() )
+# # Output:
+# # 0    HAMMER
+# # 1       SAW
+# # 2    WRENCH
+# # dtype: object
+
+# ----------------------------------------------------
+# Reshaping and Transposing
+# ----------------------------------------------------
+
+# grades = np.array([[87,96,70],[100,87,90]])
+
+# # reshape returns a view. It does not modify the original array
+# print( grades.reshape(1,6) )
+# print( grades )
+# # Output:
+# # [[ 87  96  70 100  87  90]]
+# # [[ 87  96  70]
+# #  [100  87  90]]
+
+# # resize modifies the original array's shape
+# grades.resize(1,6)
+# print( grades )
+# # Output:
+# # [[ 87  96  70 100  87  90]]
+
+# # flatten deep copies the original array's data
+# grades = np.array([[87,96,70],[100,87,90]])
+# flattened = grades.flatten()
+# print( flattened )
+# print( grades )
+# # Output:
+# # [ 87  96  70 100  87  90]
+# # [[ 87  96  70]
+# #  [100  87  90]]
+
+# # ravel returns a view
+# raveled = grades.ravel()
+# print( raveled )
+# # Output:
+# # [ 87  96  70 100  87  90]
+
+# raveled[0] = 100
+# print( raveled )
+# print( grades )
+# # Output:
+# # [100  96  70 100  87  90]
+# # [[100  96  70]
+# #  [100  87  90]]
+
+
+# # T attribute returns a transposed view
+# # transposing - rows become the columns and the columns become the rows
+# print( grades.T )
+# print( grades )
+# # Output:
+# # [[100 100]
+# #  [ 96  87]
+# #  [ 70  90]]
+# # [[100  96  70]
+# #  [100  87  90]]
+
+
+
+# grades2 = np.array([[94,77,90],[100,81,82]])
+
+# # horizontal stack
+# print( np.hstack((grades, grades2)) )  
+# # Output:
+# # [[100  96  70  94  77  90]
+# #  [100  87  90 100  81  82]]
+
+# # vertical stack
+# print( np.vstack((grades, grades2)) ) 
+# # Output:
+# # [[100  96  70]
+# #  [100  87  90]
+# #  [ 94  77  90]
+# #  [100  81  82]]
+
+# ----------------------------------------------------
+# Deep Copies
+# ----------------------------------------------------
+
+# numbers = np.arange(1,6)
+
+# numbers2 = numbers.copy()
+
+# numbers[1] *= 10
+
+# print(numbers)
+# print(numbers2)
+# # Output:
+# # [ 1 20  3  4  5]
+# # [1 2 3 4 5]
 
 # ----------------------------------------------------
 # Views: Shallow Copies
 # ----------------------------------------------------
 
+# numbers = np.arange(1,6)
 
+# print( numbers )
+
+# numbers2 = numbers.view()
+
+# print( numbers2 )
+
+# print( id(numbers) )
+# print( id(numbers2) )
+
+# numbers[1] *= 10
+# print( numbers )
+# print( numbers2 )
+
+# numbers2[1] /= 10
+# print( numbers2 )
+# print( numbers )
+
+# # Slices also create views
+# numbers2 = numbers[0:3]
+# print( numbers2 )
+
+# numbers[1] *= 20
+# print( numbers )
+# print( numbers2 )
+# # Output:
+# #[ 1 40  3  4  5]
+# #[ 1 40  3]
 
 
 # ----------------------------------------------------

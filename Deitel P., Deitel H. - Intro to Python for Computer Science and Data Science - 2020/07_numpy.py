0501600b58a29000d92b06bdcbb00caf82ca9aed
dev <valex>
2021-07-24 15:26:16 +0300

    8

--- Deitel P., Deitel H. - Intro to Python for Computer Science and Data Science - 2020/07_numpy.py
+++ Deitel P., Deitel H. - Intro to Python for Computer Science and Data Science - 2020/07_numpy.py
@@ -5,7 +5,229 @@ import pandas as pd
 # ----------------------------------------------------
 # DataFrames (enhanced two-dimensional array)
 # ----------------------------------------------------
+grades_dict = {'Wally':[87,96,70], 'Eva':[100,87,90],
+               'Sam':[94,77,90], 'Katie':[100,81,82],
+               'Bob':[83,65,85]}
 
+grades = pd.DataFrame(grades_dict)
+
+print( grades )
+# Output:
+#    Wally  Eva  Sam  Katie  Bob
+# 0     87  100   94    100   83
+# 1     96   87   77     81   65
+# 2     70   90   90     82   85
+    
+grades.index = ['Test1', 'Test2', 'Test3']
+print( grades )
+# Output:
+#        Wally  Eva  Sam  Katie  Bob
+# Test1     87  100   94    100   83
+# Test2     96   87   77     81   65
+# Test3     70   90   90     82   85
+
+print( grades['Eva'] )
+# Output:
+# Test1    100
+# Test2     87
+# Test3     90
+# Name: Eva, dtype: int64
+    
+print( grades.Sam )
+# Output:
+# Test1    94
+# Test2    77
+# Test3    90
+# Name: Sam, dtype: int64
+
+print( grades.loc['Test1'] ) # access a row RECOMMENDED
+# Wally     87
+# Eva      100
+# Sam       94
+# Katie    100
+# Bob       83
+# Name: Test1, dtype: int64
+
+print( grades.iloc[1] ) # access a row by integer(i) index RECOMMENDED
+# Wally    96
+# Eva      87
+# Sam      77
+# Katie    81
+# Bob      65
+# Name: Test2, dtype: int64
+
+# When using slices containing labels with loc, 
+# the range specified includes the high index
+print( grades.loc['Test1':'Test3'] ) 
+#        Wally  Eva  Sam  Katie  Bob
+# Test1     87  100   94    100   83
+# Test2     96   87   77     81   65
+# Test3     70   90   90     82   85
+
+# When using slices containing integer indices with iloc, 
+# the range you specify excludes the high index
+print( grades.iloc[0:2] ) 
+#        Wally  Eva  Sam  Katie  Bob
+# Test1     87  100   94    100   83
+# Test2     96   87   77     81   65
+
+# To select specific rows use list rather than slice
+print( grades.loc[['Test1','Test3']] ) 
+#        Wally  Eva  Sam  Katie  Bob
+# Test1     87  100   94    100   83
+# Test3     70   90   90     82   85
+
+print( grades.iloc[[0,2]] ) 
+#        Wally  Eva  Sam  Katie  Bob
+# Test1     87  100   94    100   83
+# Test3     70   90   90     82   85
+
+print( grades.loc['Test1':'Test2', ['Eva', 'Katie']] )
+#        Eva  Katie
+# Test1  100    100
+# Test2   87     81
+
+print( grades.iloc[[0,2], 0:3] ) 
+#        Wally  Eva  Sam
+# Test1     87  100   94
+# Test3     70   90   90
+
+print( grades[grades >= 90] ) 
+#        Wally    Eva   Sam  Katie  Bob
+# Test1    NaN  100.0  94.0  100.0  NaN
+# Test2   96.0    NaN   NaN    NaN  NaN
+# Test3    NaN   90.0  90.0    NaN  NaN
+
+print( grades[(grades>=80) & (grades < 90)] ) 
+#        Wally   Eva  Sam  Katie   Bob
+# Test1   87.0   NaN  NaN    NaN  83.0
+# Test2    NaN  87.0  NaN   81.0   NaN
+# Test3    NaN   NaN  NaN   82.0  85.0
+
+# use at and iat to get single value
+print( grades.at['Test2', 'Eva'] ) 
+# 87
+
+print( grades.iat[2, 0] ) 
+# 70
+
+grades.at['Test2', 'Eva'] = 100
+print( grades.at['Test2', 'Eva'] ) 
+# 100
+grades.iat[1, 1] = 87
+print( grades.at['Test2', 'Eva'] )
+# 87
+
+print( grades.describe() )
+#            Wally         Eva        Sam       Katie        Bob
+# count   3.000000    3.000000   3.000000    3.000000   3.000000
+# mean   84.333333   92.333333  87.000000   87.666667  77.666667
+# std    13.203535    6.806859   8.888194   10.692677  11.015141
+# min    70.000000   87.000000  77.000000   81.000000  65.000000
+# 25%    78.500000   88.500000  83.500000   81.500000  74.000000
+# 50%    87.000000   90.000000  90.000000   82.000000  83.000000
+# 75%    91.500000   95.000000  92.000000   91.000000  84.000000
+# max    96.000000  100.000000  94.000000  100.000000  85.000000
+
+pd.set_option('precision', 2)
+print( grades.describe() )
+#        Wally     Eva    Sam   Katie    Bob
+# count   3.00    3.00   3.00    3.00   3.00
+# mean   84.33   92.33  87.00   87.67  77.67
+# std    13.20    6.81   8.89   10.69  11.02
+# min    70.00   87.00  77.00   81.00  65.00
+# 25%    78.50   88.50  83.50   81.50  74.00
+# 50%    87.00   90.00  90.00   82.00  83.00
+# 75%    91.50   95.00  92.00   91.00  84.00
+# max    96.00  100.00  94.00  100.00  85.00
+
+print( grades.mean() )
+# Wally    84.33
+# Eva      92.33
+# Sam      87.00
+# Katie    87.67
+# Bob      77.67
+# dtype: float64
+
+print( grades.T ) # T returns view, not a copy
+#        Test1  Test2  Test3
+# Wally     87     96     70
+# Eva      100     87     90
+# Sam       94     77     90
+# Katie    100     81     82
+# Bob       83     65     85
+
+
+print( grades.T.describe() )
+#         Test1  Test2  Test3
+# count    5.00   5.00   5.00
+# mean    92.80  81.20  83.40
+# std      7.66  11.54   8.23
+# min     83.00  65.00  70.00
+# 25%     87.00  77.00  82.00
+# 50%     94.00  81.00  85.00
+# 75%    100.00  87.00  90.00
+# max    100.00  96.00  90.00
+
+print( grades.T.mean() )
+# Test1    92.8
+# Test2    81.2
+# Test3    83.4
+# dtype: float64
+
+# sort rows by index
+# returns copy
+print( grades.sort_index(ascending=False) )
+#        Wally  Eva  Sam  Katie  Bob
+# Test3     70   90   90     82   85
+# Test2     96   87   77     81   65
+# Test1     87  100   94    100   83
+
+# sort columns by index
+print( grades.sort_index(axis=1) ) # for rows use axis=0, default
+#        Bob  Eva  Katie  Sam  Wally
+# Test1   83  100    100   94     87
+# Test2   65   87     81   77     96
+# Test3   85   90     82   90     70
+
+# sort based on column values (axis=1) for 'Test1'
+# returns copy
+print( grades.sort_values(by='Test1', axis=1, ascending=False) ) 
+#        Eva  Katie  Sam  Wally  Bob
+# Test1  100    100   94     87   83
+# Test2   87     81   77     96   65
+# Test3   90     82   90     70   85
+
+print( grades.T.sort_values(by='Test1', ascending=False) ) 
+#        Test1  Test2  Test3
+# Eva      100     87     90
+# Katie    100     81     82
+# Sam       94     77     90
+# Wally     87     96     70
+# Bob       83     65     85
+
+print( grades.loc['Test1'].sort_values(ascending=False) )
+# Eva      100
+# Katie    100
+# Sam       94
+# Wally     87
+# Bob       83
+# Name: Test1, dtype: int64
+
+# inplace sorting
+grades.sort_index(ascending=False, inplace=True)
+print( grades )
+#        Wally  Eva  Sam  Katie  Bob
+# Test3     70   90   90     82   85
+# Test2     96   87   77     81   65
+# Test1     87  100   94    100   83
+
+grades.sort_values(by='Test1', axis=1, ascending=False, inplace=True)
+print( grades )
+#        Eva  Katie  Sam  Wally  Bob
+# Test3   90     82   90     70   85
+# Test2   87     81   77     96   65
+# Test1  100    100   94     87   83
 
 # ----------------------------------------------------
 # Series (enhanced one-dimensional array)
